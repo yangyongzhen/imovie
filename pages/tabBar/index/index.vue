0e379de178569763471de11a4e4fed72afe3b3a7
yangyongzhen <534117529@qq.com>
2024-05-31 18:38:22 +0800

    增加首页grid布局

--- pages/tabBar/index/index.vue
+++ pages/tabBar/index/index.vue
@@ -11,6 +11,34 @@
 				
 			</swiper>
 			<uni-notice-bar scrollable="true" text="这里是滚动显示的公告内容" speed:='30' style="margin-top: 5rpx;"></uni-notice-bar>
+			
+			<view class="title">
+				<view class="title-item">
+					影院热映
+				</view>
+				<view>
+					查看更多 >
+				</view>
+			</view>
+			 <scroll-view
+			     scroll-x
+			    :scroll-y="false"
+			    :show-scrollbar="false"
+			    class="scroll"
+			  >
+			    <uni-grid :column="4" :show-border="false">
+			      <uni-grid-item
+			        v-for="(item, index) in hotList"
+			        :key="index"
+			        @click="goToDetail(item.id)"
+			      >
+			        <view class="grid-item">
+			          <image :src="item.imageUrl" mode="aspectFill" />
+			          <view class="grid-item-title">{{ item.title }}</view>
+			        </view>
+			      </uni-grid-item>
+			    </uni-grid>
+			  </scroll-view>
 		</view>
 	</view>
 </template>
@@ -20,32 +48,38 @@
 	export default {
 		data() {
 			return {
-				title: 'Hello',
 				indicatorDots: true,
 				autoplay: true,
 				interval: 2000,
 				duration: 500,
 				// 轮播图的数据列表，默认为空数组
 				swiperList:[],
-			    swiperList1: [
+			    hotList: [
 			            {
 			              id: 1,
-			              imageUrl: '/static/swiper/1.jpg',
+			              imageUrl: '/static/grid/1.jpg',
 			              title: '标题1',
 			              description: '描述1',
 			            },
 			            {
 			              id: 2,
-			              imageUrl: '/static/swiper/2.jpg',
+			              imageUrl: '/static/grid/2.jpg',
 			              title: '标题2',
 			              description: '描述2',
 			            },
 			            {
 			              id: 3,
-			              imageUrl: '/static/swiper/3.jpg',
+			              imageUrl: '/static/grid/3.jpg',
 			              title: '标题3',
 			              description: '描述3',
-			            }]
+			            },
+						{
+						  id: 4,
+						  imageUrl: '/static/grid/4.jpg',
+						  title: '标题4',
+						  description: '描述4',
+						}
+						]
 			}
 		},
 		async onLoad() {
@@ -53,7 +87,9 @@
 			//console.log(this.swiperList)
 		},
 		methods: {
+			goToDetail(id){
 				
+			}
 		},
 		mounted() {
 			console.log("mounted")
@@ -63,12 +99,12 @@
 			getTop250(0,5).then(item => {
 				//this.swiperList = item;
 			});
-			
 			getNowHot(0,2,"郑州").then(result => {
 				//this.swiperList = item;
 				console.log("getNowHot,result:");
 				console.log(result);
 			});
+		
 			
 		}
 	}
@@ -86,6 +122,7 @@
 			width: 690rpx;
 			width: 100%;
 		}
+		
 	.swiper {
 		height: 300rpx;
 	}
@@ -113,8 +150,34 @@
 		text-align: center;
 	}
 
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
+	.title{
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		padding-left: 10rpx;
+		padding-right: 10rpx;
+	}
+	.title-item{
+		
+	}
+	
+	.scroll{
+		height:300rpx;
+		width: 100%;
+	}
+	
+	.grid-item {
+	  display: flex;
+	  flex-direction: column;
+	  align-items: center;
+	  justify-content: center;
+	  width: 150rpx;
+	  height: 280rpx;
+	}
+	
+	.grid-item-title {
+	  color: #606266;
+	  font-size: 12rpx;
+	  margin-top: 10rpx;
 	}
 </style>
